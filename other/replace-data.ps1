9601e7d9bb70108bab6241f89470c07cae5005d4
danijeljw-rpc <danijel@repasscloud.com>
2022-11-06 16:57:41 +1100

    airtasker fix 01

--- other/replace-data.ps1
+++ other/replace-data.ps1
@@ -5,3 +5,17 @@
         Invoke-WebRequest -Uri "https://nyc-0${int}.2qid.com/api/ordermanagement/$($i.id)" -Method Delete
     }
 }
+
+
+foreach ($i in (Invoke-WebRequest -Uri "https://engine.api.dev.optechx-data.com/v1/OrderManagement").Content | ConvertFrom-Json)
+{
+    $body = $i | ConvertTo-Json
+    try {
+        Invoke-RestMethod -Uri "https://nyc-02.2qid.com/api/ordermanagement" -Method Post -UseBasicParsing -Body $Body -ContentType "application/json" -ErrorAction Stop
+        Invoke-RestMethod -Uri "https://nyc-03.2qid.com/api/ordermanagement" -Method Post -UseBasicParsing -Body $Body -ContentType "application/json" -ErrorAction Stop
+        Invoke-RestMethod -Uri "https://nyc-04.2qid.com/api/ordermanagement" -Method Post -UseBasicParsing -Body $Body -ContentType "application/json" -ErrorAction Stop
+    }
+    catch {
+        Write-Error "$($_.Exception)"
+    }
+}
