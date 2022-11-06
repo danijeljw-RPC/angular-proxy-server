904907af6887002fa5ba05cf11bebf6c93b64654
danijeljw-rpc <danijel@repasscloud.com>
2022-11-06 16:17:08 +1100

    A new start

---
+++ other/build.sh
@@ -0,0 +1,4 @@
+docker stop aps
+docker rmi aps
+docker build --rm -t aps .
+docker run --rm --name aps -d -p 4200:4200 aps ng serve --proxy-config proxy.conf.json --host 0.0.0.0 --port 4200 --disable-host-check
